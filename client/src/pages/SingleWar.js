c2b1f5fa1358ea10d6ec1be2bae1474fba43557c
Jonathan Olmon <jonathan.olmon@gmail.com>
2021-10-14 13:51:43 -0500

    added inital modal

--- client/src/pages/SingleWar.js
+++ client/src/pages/SingleWar.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { Row, Col, Card, Tooltip, Button, Modal }  from 'antd';
 import { purple } from '@ant-design/colors';
 
-import { useParams } from 'react-router-dom';
-import { useQuery, useMutation } from '@apollo/client';
+// import { useParams } from 'react-router-dom';
+// import { useQuery, useMutation } from '@apollo/client';
 
-import { QUERY_SINGLE_WAR, ADD_TO_WAR } from '../utils/queries';
+// import { QUERY_SINGLE_WAR, ADD_TO_WAR } from '../utils/queries';
 
-import Auth from '../utils/auth';
+// import Auth from '../utils/auth';
 
 // // import CommentList from '../components/CommentList';
 // // import CommentForm from '../components/CommentForm';
@@ -17,65 +17,65 @@ import Auth from '../utils/auth';
 
 const SingleWar = () => {
 
-  const [addToWar] = useMutation(ADD_TO_WAR);
+  // const [addToWar] = useMutation(ADD_TO_WAR);
 
-  const { warId } = useParams();
+  // const { warId } = useParams();
 
-  const { loading, data } = useQuery(QUERY_SINGLE_WAR, {
-    variables: { warId: warId }
-  });
+  // const { loading, data } = useQuery(QUERY_SINGLE_WAR, {
+  //   variables: { warId: warId }
+  // });
 
-  const war = data?.war || {};
-  const users = war.users;
-  const tanks = users.filter(user => user.role === 'tank');
-  const mDPS = users.filter(user => user.role === 'mDPS');
-  const pDPS = users.filter(user => user.role === 'pDPS');
-  const eDPS = users.filter(user => user.role === 'eDPS');
-  const healers = users.filter(user => user.role === 'healer');
-  const artillery = users.filter(user => user.role === 'artillery');
+  // const war = data?.war || {};
+  // const users = war.users;
+  // const tanks = users.filter(user => user.role === 'tank');
+  // const mDPS = users.filter(user => user.role === 'mDPS');
+  // const pDPS = users.filter(user => user.role === 'pDPS');
+  // const eDPS = users.filter(user => user.role === 'eDPS');
+  // const healers = users.filter(user => user.role === 'healer');
+  // const artillery = users.filter(user => user.role === 'artillery');
 
-  const handleRegister = async (role) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+  // const handleRegister = async (role) => {
+  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
+  //   if (!token) {
+  //     return false;
+  //   }
 
-    try {
-      const { data } = await addToWar({ variables: { role } });
+  //   try {
+  //     const { data } = await addToWar({ variables: { role } });
 
-      if (!data) {
-        throw new Error("Couldn't add user to war!");
-      }
-    } catch (err) {
-      console.log(err)
-    }
-  }
+  //     if (!data) {
+  //       throw new Error("Couldn't add user to war!");
+  //     }
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
 
-  if (loading) {
-    return <div>Loading...</div>
-  }
+  // if (loading) {
+  //   return <div>Loading...</div>
+  // }
 
-  const state = {visible: false};
+  const [visible, setVisible] = React.useState(false);
+  const [confirmLoading, setConfirmLoading] = React.useState(false);
+  const [modalText, setModalText] = React.useState('Content of the modal');
 
   const showModal = () => {
-    this.setState({
-      visible: true,
-    });
+    setVisible(true);
   };
 
-  const handleOk = e => {
-    console.log(e);
-    this.setState({
-      visible: true,
-    });
+  const handleOk = () => {
+    setModalText('The modal will be closed after two seconds');
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
   };
 
-  const handleCancel = e => {
-    console.log(e);
-    this.setState({
-      visible: false,
-    });
+  const handleCancel = () => {
+    console.log('Clicked cancel button');
+    setVisible(false);
   };
 
   return (
@@ -86,86 +86,142 @@ const SingleWar = () => {
         
         <Col className="gutter-row" span={8} style={{padding: '8px 0'}}>
           <Card title="Tanks" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as a Tank" color = {purple[3]}>
-            <Button onClick={showModal()} color={purple[3]} type="primary" style={{backgroundColor: purple[3], borderColor: purple[3]}} size='small'>Register Now!</Button>
+            <Button onClick={showModal} color={purple[3]} type="primary" style={{backgroundColor: purple[3], borderColor: purple[3]}} size='small'>Register Now!</Button>
+            </Tooltip>} style={{ width: 350 }}>
             <Modal 
               title="Tank registration"
-              visible={state.visible}
+              visible={visible}
               onOk={handleOk}
+              confirmLoading={confirmLoading}
               onCancel={handleCancel}
             >
-              <p>test</p>
+              <p>{modalText}</p>
             </Modal>
+            <pre style={{fontSize: '12px'}}>Player 1:  Level 60   SS-20 | GA-18</pre>
+            <pre style={{fontSize: '12px'}}>Player 2:  Level 60   SS-20 | GA-18</pre>
+            <pre style={{fontSize: '12px'}}>Player 3:  Level 60   SS-20 | GA-18</pre>
+            {/* {tanks.map((user) => {
+              return (
+                <pre style={{fontSize: '12px'}}>{user.name}:  {user.level}   {user.primary} | {user.secondary}</pre>
+              )
+            })} */}
+          </Card>
+        </Col>
+        <Col className="gutter-row" span={8} style={{padding: '8px 0'}}>
+          <Card title="Melee DPS" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as a Melee DPS" color = {purple[3]}>
+            <Button type="primary" style={{backgroundColor: purple[3], borderColor: purple[3]}} size='small'>Register Now!</Button>
             </Tooltip>} style={{ width: 350 }}>
+            <Modal 
+              title="Melee DPS registration"
+              visible={visible}
+              onOk={handleOk}
+              confirmLoading={confirmLoading}
+              onCancel={handleCancel}
+            >
+              <p>{modalText}</p>
+            </Modal>
             <pre style={{fontSize: '12px'}}>Player 1:  Level 60   SS-20 | GA-18</pre>
             <pre style={{fontSize: '12px'}}>Player 2:  Level 60   SS-20 | GA-18</pre>
             <pre style={{fontSize: '12px'}}>Player 3:  Level 60   SS-20 | GA-18</pre>
-            {tanks.map((user) => {
+            {/* {mDPS.map((user) => {
               return (
                 <pre style={{fontSize: '12px'}}>{user.name}:  {user.level}   {user.primary} | {user.secondary}</pre>
               )
-            })}
+            })} */}
           </Card>
         </Col>
         <Col className="gutter-row" span={8} style={{padding: '8px 0'}}>
-          <Card title="Melee DPS" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as a Melee DPS" color = {purple[3]}><Button type="primary" style={{backgroundColor: purple[3], borderColor: purple[3]}} size='small'>Register Now!</Button></Tooltip>} style={{ width: 350 }}>
+          <Card title="Physical DPS" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as a Physical DPS" color = {purple[3]}>
+            <Button type="primary" style={{backgroundColor: purple[3], borderColor: purple[3]}} size='small'>Register Now!</Button>
+            </Tooltip>} style={{ width: 350 }}>
+            <Modal 
+              title="Physical DPS registration"
+              visible={visible}
+              onOk={handleOk}
+              confirmLoading={confirmLoading}
+              onCancel={handleCancel}
+            >
+              <p>{modalText}</p>
+            </Modal>
             <pre style={{fontSize: '12px'}}>Player 1:  Level 60   SS-20 | GA-18</pre>
             <pre style={{fontSize: '12px'}}>Player 2:  Level 60   SS-20 | GA-18</pre>
             <pre style={{fontSize: '12px'}}>Player 3:  Level 60   SS-20 | GA-18</pre>
-            {mDPS.map((user) => {
+            {/* {pDPS.map((user) => {
               return (
                 <pre style={{fontSize: '12px'}}>{user.name}:  {user.level}   {user.primary} | {user.secondary}</pre>
               )
-            })}
-          </Card>
-        </Col>
-        <Col className="gutter-row" span={8} style={{padding: '8px 0'}}>
-          <Card title="Physical DPS" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as a Physical DPS" color = {purple[3]}><Button type="primary" style={{backgroundColor: purple[3], borderColor: purple[3]}} size='small'>Register Now!</Button></Tooltip>} style={{ width: 350 }}>
-            <pre style={{fontSize: '12px'}}>Player 1:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 2:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 3:  Level 60   SS-20 | GA-18</pre>
-            {pDPS.map((user) => {
-              return (
-                <pre style={{fontSize: '12px'}}>{user.name}:  {user.level}   {user.primary} | {user.secondary}</pre>
-              )
-            })}
+            })} */}
           </Card>
         </Col>
       </Row>
       <Row>
         <Col className="gutter-row" span={8} style={{padding: '8px 0'}}>
-          <Card title="Elemental DPS" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as an Elemental DPS" color = {purple[3]}><Button type="primary" style={{backgroundColor: purple[3], borderColor: purple[3]}} size='small'>Register Now!</Button></Tooltip>} style={{ width: 350 }}>
+          <Card title="Elemental DPS" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as an Elemental DPS" color = {purple[3]}>
+            <Button type="primary" style={{backgroundColor: purple[3], borderColor: purple[3]}} size='small'>Register Now!</Button>
+            </Tooltip>} style={{ width: 350 }}>
+            <Modal 
+              title="Elemental DPS registration"
+              visible={visible}
+              onOk={handleOk}
+              confirmLoading={confirmLoading}
+              onCancel={handleCancel}
+            >
+              <p>{modalText}</p>
+            </Modal>
             <pre style={{fontSize: '12px'}}>Player 1:  Level 60   SS-20 | GA-18</pre>
             <pre style={{fontSize: '12px'}}>Player 2:  Level 60   SS-20 | GA-18</pre>
             <pre style={{fontSize: '12px'}}>Player 3:  Level 60   SS-20 | GA-18</pre>
-            {eDPS.map((user) => {
+            {/* {eDPS.map((user) => {
               return (
                 <pre style={{fontSize: '12px'}}>{user.name}:  {user.level}   {user.primary} | {user.secondary}</pre>
               )
-            })}
+            })} */}
           </Card>
         </Col>
         <Col className="gutter-row" span={8} style={{padding: '8px 0'}}>
-          <Card title="Healer" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as a Healer" color = {purple[3]}><Button type="primary" style={{backgroundColor: purple[3], borderColor: purple[3]}} size='small'>Register Now!</Button></Tooltip>} style={{ width: 350 }}>
+          <Card title="Healer" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as a Healer" color = {purple[3]}>
+            <Button type="primary" style={{backgroundColor: purple[3], borderColor: purple[3]}} size='small'>Register Now!</Button>
+            </Tooltip>} style={{ width: 350 }}>
+            <Modal 
+              title="Healer registration"
+              visible={visible}
+              onOk={handleOk}
+              confirmLoading={confirmLoading}
+              onCancel={handleCancel}
+            >
+              <p>{modalText}</p>
+            </Modal>
             <pre style={{fontSize: '12px'}}>Player 1:  Level 60   SS-20 | GA-18</pre>
             <pre style={{fontSize: '12px'}}>Player 2:  Level 60   SS-20 | GA-18</pre>
             <pre style={{fontSize: '12px'}}>Player 3:  Level 60   SS-20 | GA-18</pre>
-            {healers.map((user) => {
+            {/* {healers.map((user) => {
               return (
                 <pre style={{fontSize: '12px'}}>{user.name}:  {user.level}   {user.primary} | {user.secondary}</pre>
               )
-            })}
+            })} */}
           </Card>
         </Col>
         <Col className="gutter-row" span={8} style={{padding: '8px 0'}}>
-          <Card title="Artillery" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as Artillery" color = {purple[3]}><Button type="primary" style={{backgroundColor: purple[3], borderColor: purple[3]}} size='small'>Register Now!</Button></Tooltip>} style={{ width: 350 }}>
+          <Card title="Artillery" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as Artillery" color = {purple[3]}>
+            <Button type="primary" style={{backgroundColor: purple[3], borderColor: purple[3]}} size='small'>Register Now!</Button>
+            </Tooltip>} style={{ width: 350 }}>
+            <Modal 
+              title="Artillery registration"
+              visible={visible}
+              onOk={handleOk}
+              confirmLoading={confirmLoading}
+              onCancel={handleCancel}
+            >
+              <p>{modalText}</p>
+            </Modal>
             <pre style={{fontSize: '12px'}}>Player 1:  Level 60   SS-20 | GA-18</pre>
             <pre style={{fontSize: '12px'}}>Player 2:  Level 60   SS-20 | GA-18</pre>
             <pre style={{fontSize: '12px'}}>Player 3:  Level 60   SS-20 | GA-18</pre>
-            {artillery.map((user) => {
+            {/* {artillery.map((user) => {
               return (
                 <pre style={{fontSize: '12px'}}>{user.name}:  {user.level}   {user.primary} | {user.secondary}</pre>
               )
-            })}
+            })} */}
           </Card>
         </Col>
       </Row>
