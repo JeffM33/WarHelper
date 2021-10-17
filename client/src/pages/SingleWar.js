37d3f41391d2cc74b3105c79757efe20570a6c51
Jonathan Olmon <jonathan.olmon@gmail.com>
2021-10-16 21:01:13 -0500

    Updated some of the formatting and added a notification once the registration is complete.

--- client/src/pages/SingleWar.js
+++ client/src/pages/SingleWar.js
@@ -1,6 +1,6 @@
 import React,{ useState } from 'react';
 
-import { Row, Col, Card, Tooltip, Button, Modal, Select, Form }  from 'antd';
+import { Row, Col, Card, Tooltip, Button, Modal, Select, Form, notification }  from 'antd';
 import { purple } from '@ant-design/colors';
 
 import { useParams } from 'react-router-dom';
@@ -102,12 +102,17 @@ const SingleWar = () => {
     }
 
     console.log('Success!', values, 'role:', role);
-    setModalText('Thank you for registering!');
     setConfirmLoading(true);
     setTimeout(() => {
       setVisible(false);
       setConfirmLoading(false);
-    }, 2000);
+      notification['success']({
+        message: 'Registered',
+        description:
+          'Thank you for registering for the upcoming invasion!',
+        placement: 'topLeft'
+      });
+    }, 100);
     form.resetFields();
   }
 
@@ -178,88 +183,70 @@ const SingleWar = () => {
 
       <Row justify="space-between">
         
-        <Col className="gutter-row" span={8} style={{padding: '8px 0'}}>
+        <Col className="gutter-row" xs={24} md={8} style={{padding: '8px 0'}}>
           <Card title="Tanks" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as a Tank" color = {purple[3]}>
             <Button onClick={showModal} color={purple[3]} type="primary" style={{backgroundColor: purple[3], borderColor: purple[3], display: display}} size='small'><span data-role='tanks'>Register Now!</span></Button>
             </Tooltip>} style={{ width: 350 }}>
-            <pre style={{fontSize: '12px'}}>Player 1:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 2:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 3:  Level 60   SS-20 | GA-18</pre>
             {tanks.map((user) => {
               return (
-                <pre style={{fontSize: '12px'}}>{user.username}:  Level {user.charLvl}   {user.primaryWep} {user.primaryWepLvl} | {user.secondaryWep} {user.secondaryWepLvl}</pre>
+                <pre style={{fontSize: '12px'}}>{user.username}:  Level {user.charLvl}   {user.primaryWep}-{user.primaryWepLvl} | {user.secondaryWep}-{user.secondaryWepLvl}</pre>
               )
             })}
           </Card>
         </Col>
-        <Col className="gutter-row" span={8} style={{padding: '8px 0'}}>
+        <Col className="gutter-row" xs={24} md={8} style={{padding: '8px 0'}}>
           <Card title="Melee DPS" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as a Melee DPS" color = {purple[3]}>
             <Button onClick={showModal} type="primary" style={{backgroundColor: purple[3], borderColor: purple[3], display: display}} size='small'><span data-role='mdps'>Register Now!</span></Button>
             </Tooltip>} style={{ width: 350 }}>
-            <pre style={{fontSize: '12px'}}>Player 1:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 2:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 3:  Level 60   SS-20 | GA-18</pre>
             {mdps.map((user) => {
               return (
-                <pre style={{fontSize: '12px'}}>{user.username}:  Level {user.charLvl}   {user.primaryWep} {user.primaryWepLvl} | {user.secondaryWep} {user.secondaryWepLvl}</pre>
+                <pre style={{fontSize: '12px'}}>{user.username}:  Level {user.charLvl}   {user.primaryWep}-{user.primaryWepLvl} | {user.secondaryWep}-{user.secondaryWepLvl}</pre>
               )
             })}
           </Card>
         </Col>
-        <Col className="gutter-row" span={8} style={{padding: '8px 0'}}>
+        <Col className="gutter-row" xs={24} md={8} style={{padding: '8px 0'}}>
           <Card title="Physical DPS" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as a Physical DPS" color = {purple[3]}>
             <Button onClick={showModal} type="primary" style={{backgroundColor: purple[3], borderColor: purple[3], display: display}} size='small'><span data-role='prdps'>Register Now!</span></Button>
             </Tooltip>} style={{ width: 350 }}>
-            <pre style={{fontSize: '12px'}}>Player 1:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 2:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 3:  Level 60   SS-20 | GA-18</pre>
             {prdps.map((user) => {
               return (
-                <pre style={{fontSize: '12px'}}>{user.username}:  Level {user.charLvl}   {user.primaryWep} {user.primaryWepLvl} | {user.secondaryWep} {user.secondaryWepLvl}</pre>
+                <pre style={{fontSize: '12px'}}>{user.username}:  Level {user.charLvl}   {user.primaryWep}-{user.primaryWepLvl} | {user.secondaryWep}-{user.secondaryWepLvl}</pre>
               )
             })}
           </Card>
         </Col>
       </Row>
       <Row>
-        <Col className="gutter-row" span={8} style={{padding: '8px 0'}}>
+        <Col className="gutter-row" xs={24} md={8} style={{padding: '8px 0'}}>
           <Card title="Elemental DPS" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as an Elemental DPS" color = {purple[3]}>
             <Button onClick={showModal} type="primary" style={{backgroundColor: purple[3], borderColor: purple[3], display: display}} size='small'><span data-role='erdps'>Register Now!</span></Button>
             </Tooltip>} style={{ width: 350 }}>
-            <pre style={{fontSize: '12px'}}>Player 1:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 2:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 3:  Level 60   SS-20 | GA-18</pre>
             {erdps.map((user) => {
               return (
-                <pre style={{fontSize: '12px'}}>{user.username}:  Level {user.charLvl}   {user.primaryWep} {user.primaryWepLvl} | {user.secondaryWep} {user.secondaryWepLvl}</pre>
+                <pre style={{fontSize: '12px'}}>{user.username}:  Level {user.charLvl}   {user.primaryWep}-{user.primaryWepLvl} | {user.secondaryWep}-{user.secondaryWepLvl}</pre>
               )
             })}
           </Card>
         </Col>
-        <Col className="gutter-row" span={8} style={{padding: '8px 0'}}>
+        <Col className="gutter-row" xs={24} md={8} style={{padding: '8px 0'}}>
           <Card title="Healer" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as a Healer" color = {purple[3]}>
             <Button onClick={showModal} type="primary" style={{backgroundColor: purple[3], borderColor: purple[3], display: display}} size='small'><span data-role='healers'>Register Now!</span></Button>
             </Tooltip>} style={{ width: 350 }}>
-            <pre style={{fontSize: '12px'}}>Player 1:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 2:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 3:  Level 60   SS-20 | GA-18</pre>
             {healers.map((user) => {
               return (
-                <pre style={{fontSize: '12px'}}>{user.username}:  Level {user.charLvl}   {user.primaryWep} {user.primaryWepLvl} | {user.secondaryWep} {user.secondaryWepLvl}</pre>
+                <pre style={{fontSize: '12px'}}>{user.username}:  Level {user.charLvl}   {user.primaryWep}-{user.primaryWepLvl} | {user.secondaryWep}-{user.secondaryWepLvl}</pre>
               )
             })}
           </Card>
         </Col>
-        <Col className="gutter-row" span={8} style={{padding: '8px 0'}}>
+        <Col className="gutter-row" xs={24} md={8} style={{padding: '8px 0'}}>
           <Card title="Artillery" headStyle={{ fontSize: '27px' }} extra={<Tooltip title="Register for this war as Artillery" color = {purple[3]}>
             <Button onClick={showModal} type="primary" style={{backgroundColor: purple[3], borderColor: purple[3], display: display}} size='small'><span data-role='artillery'>Register Now!</span></Button>
             </Tooltip>} style={{ width: 350 }}>
-            <pre style={{fontSize: '12px'}}>Player 1:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 2:  Level 60   SS-20 | GA-18</pre>
-            <pre style={{fontSize: '12px'}}>Player 3:  Level 60   SS-20 | GA-18</pre>
             {artillery.map((user) => {
               return (
-                <pre style={{fontSize: '12px'}}>{user.username}:  Level {user.charLvl}   {user.primaryWep} {user.primaryWepLvl} | {user.secondaryWep} {user.secondaryWepLvl}</pre>
+                <pre style={{fontSize: '12px'}}>{user.username}:  Level {user.charLvl}   {user.primaryWep}-{user.primaryWepLvl} | {user.secondaryWep}-{user.secondaryWepLvl}</pre>
               )
             })}
           </Card>
